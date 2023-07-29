061f573684715c007ac0d40375cc0ac4b9fcb4b5
Instructor Kjrsten <42076843+instructorkjrsten@users.noreply.github.com>
2023-07-29 00:14:52 -0500

    testing transcript tool

--- website-2023-qa/js/app.js
+++ website-2023-qa/js/app.js
@@ -1,14 +1,14 @@
-let burger = document.querySelector(".burger");
-let navMenu = document.querySelector(".group .navigation");
+// let burger = document.querySelector(".burger");
+// let navMenu = document.querySelector(".group .navigation");
 
-burger.addEventListener("click", () =>{
-    burger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-});
+// burger.addEventListener("click", () =>{
+//     burger.classList.toggle("active");
+//     navMenu.classList.toggle("active");
+// });
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    burger.classList.remove("active");
-    navMenu.classList.remove("active");
-}));
+// document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+//     burger.classList.remove("active");
+//     navMenu.classList.remove("active");
+// }));
 
 
